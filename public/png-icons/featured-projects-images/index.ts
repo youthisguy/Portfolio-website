1c6f24437d1e06eab63de662a5e43125c02e71b5
Sam Steel <samuel.ini145@gmail.com>
2023-09-21 12:37:07 +0100

    image bug fix

--- public/png-icons/featured-projects-images/index.ts
+++ public/png-icons/featured-projects-images/index.ts
@@ -1,8 +1,8 @@
-export { default as hipnodeMobileView } from "./carhub-mobile-view.png";
+export { default as hipnodeMobileView } from "./carhub-mobile-view.jpeg";
 export { default as hipnodeDesktopView } from "./carhub-desktop-view.png";
 export { default as iphoneTemplate } from "./iphone-template.png";
 export { default as jobItMobileView } from "./immersive-mobile-view.jpeg";
-export { default as jobItDesktopView } from "./immersive-desktop-view.jpeg";
+export { default as jobItDesktopView } from "./immersive-desktop-view.png";
 export { default as laptopTemplate } from "./laptop-template.png";
 export { default as morentMobileView } from "./kitches-mobile-view.jpeg";
 export { default as morentDesktopView } from "./kitches-desktop-view.png";
